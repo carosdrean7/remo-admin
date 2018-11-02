a72fac1880154e4097cc85c9e13d0d2845174aeb
Oscar Salcedo <josueht15@gmail.com>
2018-11-02 15:40:48 -0500

    avanzando niveles

--- src/app/admin/components/niveles/niveles.component.ts
+++ src/app/admin/components/niveles/niveles.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Componente } from 'src/app/abstract/component.abstract';
+import { NivelesService } from 'src/app/providers/niveles.service';
+import { NotyfService } from 'ng-notyf';
+import { Nivel } from 'src/app/interface/interfaces.interface';
+
+declare var $: any;
 
 @Component({
   selector: 'app-niveles',
   templateUrl: './niveles.component.html',
   styles: []
 })
-export class NivelesComponent implements OnInit {
+export class NivelesComponent extends Componente implements OnInit {
 
-  constructor() { }
+  caso;
+  nivel: Nivel;
+
+  constructor(_ns: NivelesService, notif: NotyfService) {
+    super(_ns, notif);
+    this.caso = 'Nuevo';
+  }
 
   ngOnInit() {
+  }
+
+  editar(nivel: Nivel) {
+    this.caso = 'Editar';
+    console.log(nivel);
+    this._idEditar = nivel._id;
+    this.nivel = nivel;
+  }
+
+  resetearItem() {
+    this.nivel = {
+      nombre: '',
+      precio: ''
+    };
+  }
+
+  loadScript() {
+    $('.swal-overlay').on('click', function() {
+      $('.swal-overlay').removeClass('swal-overlay--show-modal');
+    });
+
   }
 
 }
--- src/app/interface/interfaces.interface.ts
+++ src/app/interface/interfaces.interface.ts
@@ -1,14 +1,20 @@
 export interface Post {
-    _id: string;
+    _id?: string;
     titulo: string;
     contenido: string;
     fechaPublicacion: string;
 }
 
 export interface Actividad {
-    _id: string;
+    _id?: string;
     titulo: string;
     fecha: string;
     tipo: string;
     descripcion: string;
+}
+
+export interface Nivel {
+    _id?: string;
+    nombre: string;
+    precio: string;
 }
